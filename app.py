2f28e701e67ef34e7e13be5b120eef189ad9edcb
Estevao-Lang <estevao09.gabriel@gmail.com>
2024-03-13 16:22:55 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
-
+import bcrypt
 app = Flask(__name__)
 
 # Configurações do banco de dados
@@ -8,7 +8,7 @@ db_config = {
     'host': 'localhost',
     'user': 'root',
     'password': '',
-    'database': 'exemplo'
+    'database': 'registro'
 }
 
 # Rota para adicionar registro
@@ -45,9 +45,65 @@ def exibir_registros():
 
     return render_template('exibir_registros.html', registros=registros)
 
+@app.route('/registro_de_login.html', methods= ['GET','POST'])
+def registro_de_login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        
+        #Encode a senha antes de aplicar hash
+        senha_encoded = senha.encode('utf-8')
+        
+        salt = bcrypt.gensalt(8)
+        hashed_password  = bcrypt.hashpw(senha_encoded,salt)
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        cursor.execute("INSERT INTO user (nome, email, senha) VALUES (%s, %s, %s)", (nome, email, hashed_password))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    return render_template('registro_de_login.html')
+
+@app.route('/login')
+def login_page():
+     return render_template('login.html')
+
 @app.route('/')
-def pagina_inicial():
-    return render_template('pagina_inicial.html', registros=[])
+def login():
+    return render_template('login.html', registros=[])
+
+@app.route('/login.html', methods=['POST', 'GET'])
+def verifica_login():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(dictionary=True)
+
+        # Use SELECT para verificar os dados de login
+        cursor.execute("SELECT * FROM user WHERE nome = %s AND email = %s", (nome, email))
+        user = cursor.fetchone()
+
+        cursor.close()
+        conn.close()
+
+        if user and bcrypt.checkpw(senha.encode('utf-8'), user['senha'].encode('utf-8')):
+            # Senha correta, redirecione para a página inicial
+            return render_template('pagina_inicial.html')
+        else:
+            # Senha incorreta, renderize a página de login novamente
+            return render_template('login.html')
+
+    # Se for um método GET, renderize a página de login novamente
+    return render_template('login.html')
+
+# ... (restante do seu código)
 
 if __name__ == '__main__':
     app.run(debug=True)
